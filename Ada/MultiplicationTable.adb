4761abf92310b5025533d0822f30e44cb2b125f8
Kangrok Kim <kimpro@uebermensch.tech>
2024-02-22 03:37:13 +0900

    Create MultiplicationTable.adb

---
+++ Ada/MultiplicationTable.adb
@@ -0,0 +1,29 @@
+-- Multiplication Table
+-- 2024.02.21
+
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+
+procedure Multiplication_Table is
+-- warning: file name does not match unit name, should be "multiplication_table.adb" [enabled by default]
+begin
+    for Group_Index in 0..2 loop
+        for Multiplier in 1..9 loop
+            for Position_In_Group in 1..3 loop
+                declare
+                    Multiplicand : Integer := Group_Index * 3 + Position_In_Group;
+                    Result : Integer := Multiplicand * Multiplier;
+                begin
+                    Put(Multiplicand, 2);
+                    Put(" * ");
+                    Put(Multiplier, 2);
+                    Put(" = ");
+                    Put(Result, 2);
+                    Put("   ");
+                end;
+            end loop;
+            New_Line;
+        end loop;
+        New_Line;
+    end loop;
+end Multiplication_Table;
